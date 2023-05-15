76b5b4181fda1639ca88b8aa0798458f0bbf5e2d
Swappnet <advance11ua@gmail.com>
2023-05-15 15:45:32 +0300

    initial app setup

---
+++ config/site.ts
@@ -0,0 +1,10 @@
+import { SiteConfig } from '@/types/global/siteConfig.types';
+
+export const siteConfig: SiteConfig = {
+  name: 'rhcraft',
+  description: 'Website for minecraft server',
+  url: 'https://rhcraft.vercel.app',
+  links: {
+    github: 'https://github.com/swappnet/rhcraft',
+  },
+};

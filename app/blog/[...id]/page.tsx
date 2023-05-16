12ec2e2a6c0147a2cc4e2d5d2218e6a264fa45ff
Swappnet <advance11ua@gmail.com>
2023-05-16 20:03:40 +0300

    added basic db fetching

---
+++ app/blog/[...id]/page.tsx
@@ -0,0 +1,46 @@
+import { db } from '@/lib/db'
+import { notFound } from 'next/navigation'
+
+interface PostPageProps {
+  params: {
+    id: string[]
+  }
+}
+
+async function getPostFromParams(params: any) {
+  const id = params?.id?.join('/')
+
+  const post = await db.post.findUnique({
+    where: {
+      id: id,
+    },
+  })
+
+  if (!post) {
+    null
+  }
+
+  return post
+}
+
+export default async function PostPage({ params }: PostPageProps) {
+  const post = await getPostFromParams(params)
+
+  if (!post) {
+    notFound()
+  }
+
+  return (
+    <>
+      <p>Post detail page</p>
+      <ul>
+        {post && (
+          <li key={post.id}>
+            <p>Post id: {post.id}</p>
+            <p>Post title: {post.title}</p>
+          </li>
+        )}
+      </ul>
+    </>
+  )
+}

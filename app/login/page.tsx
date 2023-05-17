b028cef6337ba88dd2ebdef23cf888c19c32f477
Swappnet <advance11ua@gmail.com>
2023-05-17 15:15:58 +0300

    added redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,14 @@
 import { AuthForm } from '@/components/auth-form'
+import { getCurrentUser } from '@/lib/session'
+import { redirect } from 'next/navigation'
 
 export default async function Login() {
+  const user = await getCurrentUser()
+
+  if (user) {
+    redirect('/dashboard')
+  }
+
   return (
     <>
       <p>Login page</p>

2c5d447b8af37d83505d99144156942b560f9963
Swappnet <advance11ua@gmail.com>
2023-05-18 13:52:35 +0300

    changed styling

--- components/container.tsx
+++ components/container.tsx
@@ -7,11 +7,11 @@ type Container = {
 
 const Container = ({ variant, children }: Container) => {
   return variant === 'page' ? (
-    <div className="w-full h-full min-h-screen mx-auto my-0 flex flex-col justify-start items-center">
+    <div className="w-full h-full min-h-[90vh] mx-auto my-0 flex flex-col justify-start items-center">
       {children}
     </div>
   ) : variant === 'page-content' ? (
-    <main className="w-full h-full min-h-screen max-w-[80rem] mx-auto my-0 p-4 pt-16 flex flex-col items-center justify-start ">
+    <main className="w-full h-full min-h-[90vh] max-w-[80rem] mx-auto my-0 p-4 pt-16 flex flex-col items-center justify-start ">
       {children}
     </main>
   ) : (

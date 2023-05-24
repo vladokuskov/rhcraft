7c69d4b6f44789e1c9a330622d29ec5daccb82e0
Swappnet <advance11ua@gmail.com>
2023-05-24 12:03:05 +0300

    added basic ui for post component

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
       <section className="w-full flex items-start justify-center gap-8 max-lg:flex-col-reverse mt-8">
         <div className="relative w-full flex items-center justify-center ">
           <Image
-            className="rounded relative border-2 border-inherit border-neutral-300"
+            className="rounded relative"
             src={bannerImage}
             height={400}
             alt="Banner picture"
@@ -24,7 +24,7 @@ export default async function Home() {
         </div>
         <div className="w-full flex flex-col items-start justify-start gap-4">
           <h1 className=" text-5xl font-sans font-bold flex flex-col items-start justify-start text-white-100">
-            WELCOME TO<span className=" text-yellow-400">RHCraft!</span>
+            WELCOME TO<span className=" text-[#E4DE39]">RHCraft!</span>
           </h1>
           <p className=" max-w-[28rem] font-sans text-lg text-neutral-500 font-medium font-semibold leading-4">
             the ultimate Minecraft server for survival. We offer a unique and
@@ -32,7 +32,7 @@ export default async function Home() {
           </p>
         </div>
       </section>
-      <section className="w-full flex flex-col items-start justify-center gap-8 mt-8">
+      <section className="w-full flex flex-col items-start justify-center gap-8 mt-8 mb-12">
         <div className="w-full flex flex-col items-start justify-start gap-1">
           <h2 className=" text-4xl font-sans font-bold flex flex-col items-start justify-start text-white-100">
             Latest news

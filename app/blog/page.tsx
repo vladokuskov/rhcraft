18852e47ead2c841d8a6e44894aede880444dab3
Swappnet <advance11ua@gmail.com>
2023-05-26 18:56:47 +0300

    added skeleton

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,7 @@
 import BlogSidebar from '@/components/blog-sidebar'
 import BlogPostsList from '@/components/blog/blog-posts-list'
+import BlogPostsLoading from '@/components/blog/skeletons/blog-posts-skeleton'
+import { Suspense } from 'react'
 
 export const metadata = {
   title: 'rhcraft - Blog',
@@ -15,7 +17,9 @@ export default async function Blog() {
     <div className="w-full h-full min-h-screen mx-auto my-0 grid grid-cols-withSidebar max-sm:flex max-sm:gap-8 flex-col items-start justify-center gap-16">
       <BlogSidebar />
       <div className="w-full h-1 w-full h-full min-h-screen flex flex-col items-start justify-start">
-        <BlogPostsList />
+        <Suspense fallback={<BlogPostsLoading />}>
+          <BlogPostsList />
+        </Suspense>
       </div>
     </div>
   )

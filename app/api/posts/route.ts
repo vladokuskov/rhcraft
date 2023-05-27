07723a2565b315519bc226e2828bb493294c4f4c
Swappnet <advance11ua@gmail.com>
2023-05-27 10:09:19 +0300

    added infinite scroll

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,18 +3,65 @@ import { db } from '@/lib/db'
 import { getServerSession } from 'next-auth'
 import { z } from 'zod'
 
-export async function GET() {
+export async function GET(req: Request) {
   try {
-    const posts = await db.post.findMany({
+    const url = new URL(req.url)
+
+    const take = url.searchParams.get('take')
+    const lastCursor = url.searchParams.get('lastCursor')
+
+    let result = await db.post.findMany({
       where: {
         published: true,
       },
+      take: take ? parseInt(take as string) : 10,
+      ...(lastCursor && {
+        skip: 1,
+        cursor: {
+          id: lastCursor as string,
+        },
+      }),
       orderBy: {
         createdAt: 'desc',
       },
     })
 
-    return new Response(JSON.stringify(posts))
+    if (result.length == 0) {
+      return new Response(
+        JSON.stringify({
+          data: [],
+          metaData: {
+            lastCursor: null,
+            hasNextPage: false,
+          },
+        }),
+        { status: 200 },
+      )
+    }
+
+    const lastPostInResults: any = result[result.length - 1]
+    const cursor: any = lastPostInResults.id
+
+    const nextPage = await db.post.findMany({
+      where: {
+        published: true,
+      },
+      take: take ? parseInt(take as string) : 7,
+      skip: 1,
+      cursor: {
+        id: cursor,
+      },
+    })
+
+    const data = {
+      data: result,
+      metaData: {
+        lastCursor: cursor,
+        hasNextPage: nextPage.length > 0,
+      },
+    }
+
+    return new Response(JSON.stringify(data), { status: 200 })
   } catch (error) {
     return new Response(null, { status: 500 })
   }
---
+++ providers/QueryProvider.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+interface Props {
+  children: React.ReactNode
+}
+
+const queryClient = new QueryClient()
+
+const QueryProvider = ({ children }: Props) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
+
+export default QueryProvider

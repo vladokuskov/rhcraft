410c391d45f7544fd0ac04c94bdefa4469a6e837
Swappnet <advance11ua@gmail.com>
2023-05-31 12:09:10 +0300

    refactored link component, added back link

--- components/navbar/nav-menu.tsx
+++ components/navbar/nav-menu.tsx
@@ -38,17 +38,23 @@ const NavBarMenu = () => {
         <div className="absolute m-auto left-0 right-0 w-full bg-neutral-700 p-3 py-6 top-12 flex flex-col gap-5 tap border-b border-white border-opacity-10">
           <div className="flex flex-col gap-4 w-full px-3">
             <NavigationLink
-              destination="/"
-              title="HOME"
-              icon={faAngleRight}
               variant="nav"
-            />
+              href="/"
+              className=" justify-between"
+              disabled={pathname === '/'}
+              title="Home"
+            >
+              HOME <FontAwesomeIcon icon={faAngleRight} />
+            </NavigationLink>
             <NavigationLink
-              destination="/blog"
-              title="BLOG"
-              icon={faAngleRight}
               variant="nav"
-            />
+              href="/blog"
+              className=" justify-between"
+              disabled={pathname === '/blog'}
+              title="Blog"
+            >
+              BLOG <FontAwesomeIcon icon={faAngleRight} />
+            </NavigationLink>
           </div>
         </div>
       )}
--- components/navigation-link.tsx
+++ components/navigation-link.tsx
@@ -1,58 +1,51 @@
 'use client'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import { IconDefinition } from '@fortawesome/free-regular-svg-icons'
+import Link, { LinkProps } from 'next/link'
 import clsx from 'clsx'
+import { ReactNode } from 'react'
 
-type NavigationLink = {
-  title: string
-  destination: string
-  variant?: 'nav' | 'default' | 'dashboard'
-  icon?: IconDefinition
+interface HTMLAnchorTitleProps {
+  title?: string
+}
+
+type linkVariants = 'regular' | 'nav' | 'dashboard'
+
+interface NavigationLinkProps extends LinkProps, HTMLAnchorTitleProps {
+  variant?: linkVariants
   className?: string
+  children: ReactNode
+  disabled?: boolean
 }
 
 const NavigationLink = ({
-  title,
-  destination,
-  variant = 'default',
-  icon,
+  variant = 'regular',
   className,
-}: NavigationLink) => {
-  const pathname = usePathname()
-
+  children,
+  disabled = false,
+  ...props
+}: NavigationLinkProps) => {
   return (
     <Link
-      href={destination}
-      title={title}
-      aria-disabled={
-        (variant === 'nav' || variant === 'dashboard') &&
-        pathname === destination
-      }
       className={clsx(
-        'w-full inline-flex hover:transition-colors ',
-        {
-          'font-sans tracking-wider text-lg font-bold text-white-100 justify-between items-center aria-disabled:text-neutral-500':
-            variant === 'nav',
-          'font-inter hover:underline underline-offset-2 text-white-100 justify-between items-center hover:text-neutral-300 focus::text-neutral-300':
-            variant === 'default',
-          'gap-2 flex-row-reverse items-center justify-end w-full rounded text-white p-1 font-sans font-bold tracking-wide text-lg hover:text-neutral-300 focus::text-neutral-300':
-            variant === 'dashboard',
-          'cursor-default': variant === 'nav' && pathname === destination,
-          'cursor-default bg-neutral-600 !text-neutral-300':
-            variant === 'dashboard' && pathname === destination,
-        },
+        'inline-flex gap-2 hover:transition-colors items-center',
+        variant === 'nav' &&
+          `font-sans text-lg text-white-100 tracking-wider font-bold hover:text-neutral-300 focus:text-neutral-300 ${
+            disabled &&
+            'text-neutral-500 hover:text-neutral-500 focus:text-neutral-500 pointer-events-none'
+          }`,
+        variant === 'regular' &&
+          'font-inter text-white-100 underline-offset-2 hover:text-neutral-300 hover:underline focus:underline focus:text-neutral-300',
+        variant === 'dashboard' &&
+          `font-sans text-white  text-lg p-1 rounded font-bold tracking-wide hover:text-neutral-300 focus:text-neutral-300 ${
+            disabled &&
+            'bg-neutral-700 !text-neutral-400 hover:text-neutral-400 focus:text-neutral-400 pointer-events-none '
+          }`,
+
         className,
       )}
+      {...props}
     >
-      {title}
-      {icon && (
-        <span>
-          <FontAwesomeIcon icon={icon} />
-        </span>
-      )}
+      {children}
     </Link>
   )
 }

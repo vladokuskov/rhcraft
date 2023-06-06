8a61360688ce46fafe449d9d6a268be5a1e9ce5d
Swappnet <advance11ua@gmail.com>
2023-06-06 10:35:00 +0300

    loading fix

--- components/dashboard/dashboard-posts-list.tsx
+++ components/dashboard/dashboard-posts-list.tsx
@@ -14,7 +14,7 @@ type PostsQueryParams = {
 }
 
 const allPosts = async ({ take, lastCursor }: PostsQueryParams) => {
-  const response = await axios.get('/api/posts', {
+  const response = await axios.get('/api/posts/dashboard', {
     params: { take, lastCursor },
   })
   return response?.data
